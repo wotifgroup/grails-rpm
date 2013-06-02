0188d2eafa0af1e85d6474406ae6d81363e9db7e
Adam Harwood <adam.harwood@wotifgroup.com>
2013-06-03 07:51:56 +1000

    Cleanup

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,15 +1,14 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 
 grails.project.dependency.resolution = {
     inherits("global")
     log "warn"
-    legacyResolve false
     repositories {
         grailsCentral()
+        mavenLocal()
         mavenCentral()
     }
+
     dependencies {
         build "org.redline-rpm:redline:1.1.12",
                 "commons-io:commons-io:2.4"
